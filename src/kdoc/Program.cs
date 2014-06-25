1341ee578d018e6ffd160761e9dd7c9f1e0fc692
DamianEdwards <damian@damianedwards.com>
2014-06-25 10:05:25 -0700

    Added a solution with console app

---
+++ src/kdoc/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace kdoc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("kdoc will do amazing things...");
+            Console.ReadLine();
+        }
+    }
+}
