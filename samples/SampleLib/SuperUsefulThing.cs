5a1ceeca559e7b2c2310926962c7be11fa84e799
DamianEdwards <damian@damianedwards.com>
2014-06-25 15:50:31 -0700

    Added more stuff to SampleLib

--- samples/SampleLib/SuperUsefulThing.cs
+++ samples/SampleLib/SuperUsefulThing.cs
@@ -17,11 +17,26 @@ namespace SampleLib
         public delegate void HelloSayEventHandler(object sender, EventArgs args);
 
         /// <summary>
-        /// Creates a new SuperUsefulThing.
+        /// The default salutation.
+        /// </summary>
+        public static readonly string DefaultSalutation = "Hello";
+
+        /// <summary>
+        /// Creates a new SuperUsefulThing using the default salutation (<see cref="DefaultSalutation" />).
         /// </summary>
         public SuperUsefulThing()
+            : this(DefaultSalutation)
         {
-            Salutation = "Hello";
+            
+        }
+
+        /// <summary>
+        /// Creates a new SuperUsefulThing.
+        /// </summary>
+        /// <param name="salutation">The salutation to use.</param>
+        public SuperUsefulThing(string salutation)
+        {
+            Salutation = salutation;
         }
 
         /// <summary>
@@ -30,14 +45,42 @@ namespace SampleLib
         public string Salutation { get; set; }
 
         /// <summary>
+        /// Raised when <see cref="SayHello(string)"/> is called.
+        /// </summary>
+        public event EventHandler Hello;
+
+        /// <summary>
         /// Says 'Hello'.
         /// </summary>
         /// <param name="name">The name of the person to say hello to.</param>
         /// <returns>The hello message.</returns>
-        /// <example ref="docs\apiref\SampleLib\SetupUsefulThing.SayHello.md" />
         public string SayHello(string name)
         {
-            return string.Format("{0} {1}", Salutation, name);
+            return SayHello(name, yell: false);
+        }
+
+        /// <summary>
+        /// Says 'Hello'.
+        /// </summary>
+        /// <param name="name">The name of the person to say hello to.</param>
+        /// <param name="yell">A <see cref="bool"/> indicating whether the message should be loud.</param>
+        /// <returns>The hello message.</returns>
+        public string SayHello(string name, bool yell)
+        {
+            OnHello();
+            var message = string.Format("{0} {1}", Salutation, name);
+            return yell ? message.ToUpper() : message;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected virtual void OnHello()
+        {
+            if (Hello != null)
+            {
+                Hello(this, EventArgs.Empty);
+            }
         }
     }
 }
