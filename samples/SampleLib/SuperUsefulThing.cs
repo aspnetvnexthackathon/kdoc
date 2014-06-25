a4d352bdc3eff250c3a0552c4b54669924160f3b
DamianEdwards <damian@damianedwards.com>
2014-06-25 10:44:04 -0700

    Added sample lib

---
+++ samples/SampleLib/SuperUsefulThing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SampleLib
+{
+    /// <summary>
+    /// Represents a thing that does a useful thing.
+    /// </summary>
+    public class SuperUsefulThing
+    {
+        /// <summary>
+        /// Creates a new SuperUsefulThing.
+        /// </summary>
+        public SuperUsefulThing()
+        {
+
+        }
+
+        /// <summary>
+        /// Says 'Hello'.
+        /// </summary>
+        /// <param name="name">The name of the person to say hello to.</param>
+        /// <returns>The hello message.</returns>
+        public string SayHello(string name)
+        {
+            return string.Format("Hello {0}", name);
+        }
+    }
+}
---
+++ samples/SampleLib/SuperUsefulThingExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SampleLib
+{
+    /// <summary>
+    /// Extesion methods for <see cref="SampleLib.SuperUsefulThing" />
+    /// </summary>
+    public static class SuperUsefulThingExtensions
+    {
+        /// <summary>
+        /// Says 'Goodbye'.
+        /// </summary>
+        /// <param name="name">The name of the person to say goodbye to.</param>
+        /// <returns>The goodbye message.</returns>
+	    public static string SayGoodbye(string name)
+        {
+            return string.Format("Goodbye {0}", name);
+        }
+    }
+}
