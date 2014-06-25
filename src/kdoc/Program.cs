d7c55682539398f6673e2cf3189d59518a25cb2f
David Fowler <davidfowl@gmail.com>
2014-06-25 13:36:44 -0700

    Add basic roslyn model

--- src/kdoc/Program.cs
+++ src/kdoc/Program.cs
@@ -1,20 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.Framework.Runtime;
+using Microsoft.Framework.Runtime.FileSystem;
+using Microsoft.Framework.Runtime.Roslyn;
 
 namespace kdoc
 {
     public class Program
     {
         private readonly IApplicationEnvironment _appEnvironment;
+        private readonly ILibraryExportProvider _exportProvider;
+        private readonly IProjectResolver _resolver;
 
-        public Program(IApplicationEnvironment appEnvironment)
+        public Program(IApplicationEnvironment appEnvironment, 
+                       ILibraryExportProvider exportProvider,
+                       IProjectResolver resolver)
         {
             _appEnvironment = appEnvironment;
+            _exportProvider = exportProvider;
+            _resolver = resolver;
         }
 
         public void Main(string[] args)
         {
-            Console.WriteLine(string.Format("kdoc will do amazing things for {0} in {1}", _appEnvironment.ApplicationName, _appEnvironment.ApplicationBasePath));
+            var compiler = new RoslynCompiler(_resolver, NoopWatcher.Instance, _exportProvider);
+            var compilationContext = compiler.CompileProject(_appEnvironment.ApplicationName, _appEnvironment.TargetFramework);
+
+            // Build the model here
+            // compilationContext.Compilation.GlobalNamespace.Accept();
+
             Console.ReadLine();
         }
     }
